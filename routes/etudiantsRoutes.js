31263da5c4b74f25f8601b3db76a110e6dcdf83b
Celvincy04 <2438710@bdeb.qc.ca>
2026-05-08 16:26:09 -0400

    Fixe de connection dans Éleve et fixe du CSS

--- routes/etudiantsRoutes.js
+++ routes/etudiantsRoutes.js
@@ -1,9 +1,11 @@
 // Fait par Nicolas
 const express = require("express");
-
 const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleware");
 
 const eleveController = require("../controllers/etudiantControllers");
+
+router.use(authMiddleware);
 
 router.get("/Eleve", eleveController.getEleve);
 router.post("/Eleve", eleveController.addEleve);
