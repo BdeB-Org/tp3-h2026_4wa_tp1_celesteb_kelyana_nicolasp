511782f13c62718c463e3fecad278a58b670ffea
Celvincy04 <2438710@bdeb.qc.ca>
2026-05-09 20:31:05 -0400

    Ajustement du Database!

--- config/database.js
+++ config/database.js
@@ -18,6 +18,35 @@ db.serialize(() => {
     `);
 
     db.run(`
+        CREATE TABLE IF NOT EXISTS Projets (
+                id_projet INTEGER PRIMARY KEY AUTOINCREMENT,
+                titre TEXT NOT NULL,
+                description TEXT,
+                date_creation DATE,
+                id_eleve INTEGER NOT NULL,
+                id_type INTEGER NOT NULL,
+        )
+    `);
+
+    db.run(`
+        CREATE TABLE TypeProjet (
+        id_type INTEGER PRIMARY KEY AUTOINCREMENT,
+        nom_type TEXT NOT NULL UNIQUE
+        )
+    `);
+
+    db.run(`
+       CREATE TABLE ImageProjet (
+        id_image INTEGER PRIMARY KEY AUTOINCREMENT,
+        chemin_image TEXT NOT NULL,
+        id_projet INTEGER NOT NULL,
+    
+    FOREIGN KEY (id_projet) REFERENCES Projet(id_projet)
+)
+    `);
+
+
+    db.run(`
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             username TEXT UNIQUE,
