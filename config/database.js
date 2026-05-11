7f59dbb878ef0a5723378d8d45084722a8571299
Niolde <pagenicolas8@gmail.com>
2026-05-11 01:16:39 -0400

    réparation de database pour que les db.run soit actif et serveur commence

--- config/database.js
+++ config/database.js
@@ -1,7 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
-
 const db = new sqlite3.Database('./BDsql_TP1_CelesteB_KelyanA_NicolasP.db', (err) => {
- if (err) {
+    if (err) {
         console.error('Erreur SQLite :', err.message);
     } else {
         console.log('Connecté à SQLite');
@@ -12,11 +11,11 @@ db.serialize(() => {
     db.run(`
         CREATE TABLE IF NOT EXISTS Eleve (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
+            prenom TEXT,
             nom TEXT,
             programme TEXT
         )
     `);
-
     db.run(`
         CREATE TABLE IF NOT EXISTS Projet (
             id_projet INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -24,28 +23,23 @@ db.serialize(() => {
             description TEXT,
             date_creation DATE,
             id_eleve INTEGER NOT NULL,
-            id_type INTEGER NOT NULL,
+            id_type INTEGER NOT NULL
         )
     `);
-
     db.run(`
-        CREATE TABLE TypeProjet (
-           id_type INTEGER PRIMARY KEY AUTOINCREMENT,
-           nom_type TEXT NOT NULL UNIQUE
+        CREATE TABLE IF NOT EXISTS TypeProjet (
+            id_type INTEGER PRIMARY KEY AUTOINCREMENT,
+            nom_type TEXT NOT NULL UNIQUE
         )
     `);
-
     db.run(`
-        CREATE TABLE ImageProjet (
-           id_image INTEGER PRIMARY KEY AUTOINCREMENT,
-           chemin_image TEXT NOT NULL,
-           id_projet INTEGER NOT NULL,
-    
-     FOREIGN KEY (id_projet) REFERENCES Projet(id_projet)
- )
-     `);
-
-
+        CREATE TABLE IF NOT EXISTS ImageProjet (
+            id_image INTEGER PRIMARY KEY AUTOINCREMENT,
+            chemin_image TEXT NOT NULL,
+            id_projet INTEGER NOT NULL,
+            FOREIGN KEY (id_projet) REFERENCES Projet(id_projet)
+        )
+    `);
     db.run(`
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -53,7 +47,6 @@ db.serialize(() => {
             password TEXT
         )
     `);
-
     db.run(
         "INSERT OR IGNORE INTO users (username, password) VALUES (?, ?)",
         ['admin', 'admin123']
