b6c40048e93d60423cb2e4e8f11331b9504b62cf
Niolde <pagenicolas8@gmail.com>
2026-05-10 23:49:33 -0400

    connection des api dans server

--- server.js
+++ server.js
@@ -9,15 +9,18 @@ require('./config/database');
 app.use(express.json());
 app.use(express.static('public'));
 
-// const projetRoutes = require("./routes/projetRoutes");
+const projetRoutes = require("./routes/projetRoutes");
 const eleveRoutes = require("./routes/etudiantsRoutes");
-// const typesprojetRoutes = require("./routes/typesprojetRoutes");
-// const imagesRoutes = require("./routes/imagesRoutes");
+const typesprojetRoutes = require("./routes/typesprojetRoutes");
+const imagesRoutes = require("./routes/imagesRoutes");
 const authRoutes = require('./routes/authRoutes');
 
 app.use('/api/auth', authRoutes);
 
 app.use('/api', eleveRoutes);
+app.use('/api/TypeProjet', typesprojetRoutes);
+app.use('/api/Projet', projetRoutes);
+app.use('/api/Images', imagesRoutes);
 
 // Redirection par défaut
 app.get('/', (req, res) => {
